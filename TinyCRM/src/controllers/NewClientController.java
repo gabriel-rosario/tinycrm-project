6e2c02e487734ca35d170c59da42c3dc302e4bf9
Gabriel Rosario <gabriel.rosario2@upr.edu>
2018-01-13 15:11:58 -0400

    Client - phone validation works.  Contact - name and phone validation

---
+++ TinyCRM/src/controllers/NewClientController.java
@@ -0,0 +1,85 @@
+package controllers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import exceptions.InvalidFormFieldData;
+import models.CRMModel;
+import swingViews.SwingView;
+import views.ClientTCRMView;
+
+public class NewClientController extends ClientController{
+	
+	public static final Pattern VALID_PHONE_NUMBER = Pattern.compile("[0-9]+");
+	public static final Pattern VALID_EMAIL = Pattern.compile("\"\\\\b[\\\\w.%-]+@[-.\\\\w]+\\\\.[A-Za-z]{2,4}\\\\b\"",Pattern.CASE_INSENSITIVE);
+
+	public NewClientController(SwingView view, CRMModel model) {
+		super(view, model);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@Override
+	public void validateForm() throws InvalidFormFieldData {
+		getValidationErrors().clear();
+		validateCompany();
+		validateTelephone();
+		validateEmail();
+		validateWebsite();
+		validateFacebook();
+		if (getValidationErrors().size() > 0)
+			throw new InvalidFormFieldData ("Invalid Form");
+	}
+	
+	@Override
+	public void validateCompany() {
+		ClientTCRMView view = (ClientTCRMView) getView();
+		if (view.getTextCompany().trim().length() == 0) {
+			addValidationError("Company", "Empty Company. Required Field.");
+		}
+	}
+	
+	@Override
+	public void validateTelephone() {
+		ClientTCRMView view = (ClientTCRMView) getView();
+		Matcher numMatcher = VALID_PHONE_NUMBER.matcher(view.getTextTelephone());
+		boolean valid = numMatcher.matches();
+		
+		if (view.getTextTelephone().trim().length() == 0) {
+			addValidationError("Telephone", "Empty Telephone. Required Field.");
+		}else if(!valid){
+			addValidationError("Telephone", "Number Entered is not valid. Enter only digits");
+		}
+	}
+	
+	@Override
+	public void validateEmail() {
+		ClientTCRMView view = (ClientTCRMView) getView();
+		Matcher emailMatcher = VALID_EMAIL.matcher(view.getTextEmail());
+		boolean valid = emailMatcher.matches();
+		
+		if (view.getTextEmail().trim().length() == 0) {
+			addValidationError("Email", "Empty Email. Required Field.");
+		}
+//			else if(!valid) {
+//			addValidationError("Email", "Invalid Email. Please enter another.");
+//		}
+	}
+	
+	@Override
+	public void validateWebsite() {
+		ClientTCRMView view = (ClientTCRMView) getView();
+		if (view.getTextWebsite().trim().length() == 0) {
+			addValidationError("Website", "Empty Website. Required Field.");
+		}
+	}
+	
+	@Override
+	public void validateFacebook() {
+		ClientTCRMView view = (ClientTCRMView) getView();
+		if (view.getTextFacebook().trim().length() == 0) {
+			addValidationError("Facebook", "Empty Facebook. Required Field.");
+		}
+	}
+
+}
---
+++ TinyCRM/src/controllers/NewContactController.java
@@ -0,0 +1,96 @@
+package controllers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import exceptions.InvalidFormFieldData;
+import models.CRMModel;
+import swingViews.SwingView;
+import views.ContactTCRMView;
+
+public class NewContactController extends ContactController{
+
+	public NewContactController(SwingView contactView, CRMModel contactModel, CRMModel clientModel) {
+		super(contactView, contactModel, clientModel);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static final Pattern VALID_NAME = Pattern.compile("^[a-zA-Z\\s]+");
+	public static final Pattern VALID_PHONE_NUMBER = Pattern.compile("[0-9]+");
+
+
+	@Override
+	public void validateForm() throws InvalidFormFieldData {
+		getValidationErrors().clear();
+		validateFirstName();
+		validateLastName();
+		validateCompany();
+		validateTelephone();
+		validateEmail();
+		validateFacebook();
+		if (getValidationErrors().size() > 0)
+			throw new InvalidFormFieldData ("Invalid Form");
+	}
+
+	@Override
+	public void validateFirstName() throws InvalidFormFieldData {
+		ContactTCRMView view = (ContactTCRMView) getView();
+		Matcher nameMatcher = VALID_NAME.matcher(view.getTextFirstName());
+		boolean valid = nameMatcher.matches();
+
+		if (view.getTextFirstName().trim().length() == 0) {
+			addValidationError("FirstName", "Empty First Name. Required Field.");
+		}else if(!valid) {
+			addValidationError("FirstName", "Invalid First Name. It should only contain letters");
+		}
+	}
+	
+	@Override
+	public void validateLastName() throws InvalidFormFieldData {
+		ContactTCRMView view = (ContactTCRMView) getView();
+		Matcher nameMatcher = VALID_NAME.matcher(view.getTextLastName());
+		boolean valid = nameMatcher.matches();
+		
+		if (view.getTextLastName().trim().length() == 0) {
+			addValidationError("LastName", "Empty Last Name. Required Field.");
+		}else if(!valid) {
+			addValidationError("LastName", "Invalid Last Name. It should only contain letters");
+		}
+	}	
+	
+	@Override
+	public void validateCompany() throws InvalidFormFieldData {
+		ContactTCRMView view = (ContactTCRMView) getView();
+		if (view.getTextCompany().trim().length() == 0) {
+			addValidationError("Company", "Empty Company. Required Field.");
+		}
+	}	
+	
+	@Override
+	public void validateTelephone() throws InvalidFormFieldData {
+		ContactTCRMView view = (ContactTCRMView) getView();
+		Matcher numMatcher = VALID_PHONE_NUMBER.matcher(view.getTextTelephone());
+		boolean valid = numMatcher.matches();
+		if (view.getTextTelephone().trim().length() == 0) {
+			addValidationError("Telephone", "Empty Telephone. Required Field.");
+		}else if(!valid) {
+			addValidationError("Telephone", "Invalid number. It should only contain numbers.");
+		}
+	}
+	
+	@Override
+	public void validateEmail() throws InvalidFormFieldData {
+		ContactTCRMView view = (ContactTCRMView) getView();
+		if (view.getTextEmail().trim().length() == 0) {
+			addValidationError("Email", "Empty Email. Required Field.");
+		}
+	}
+	
+	@Override
+	public void validateFacebook() throws InvalidFormFieldData {
+		ContactTCRMView view = (ContactTCRMView) getView();
+		if (view.getTextFacebook().trim().length() == 0) {
+			addValidationError("Facebook", "Empty Facebook. Required Field.");
+		}
+	}
+}
